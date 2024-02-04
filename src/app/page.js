5541be5768c07f1e7987ecaa823cdb5459527332
Brickhu <brickhu@126.com>
2024-02-04 15:47:33 +0800

    集成supabase和auth

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,18 @@
-import Image from "next/image";
+import AuthForm from "@/components/auth-form"
 
 export default function Home() {
   return (
-    <main className="container">
-      DaaS.ing(dev)
-    </main>
-  );
+    <div className="row">
+      <div className="col-6">
+        <h1 className="header">Supabase Auth + Storage</h1>
+        <p>
+          Experience our Auth and Storage through a simple profile management example. Create a user
+          profile and upload an avatar image. Fast, simple, secure.
+        </p>
+      </div>
+      <div className="col-6 auth-widget">
+        <AuthForm />
+      </div>
+    </div>
+  )
 }
