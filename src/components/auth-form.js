6c06eec7292d3e4eef0735012f77c490108c6689
Brickhu <brickhu@126.com>
2024-02-05 22:01:23 +0800

    Update auth-form.js

--- src/components/auth-form.js
+++ src/components/auth-form.js
@@ -5,16 +5,14 @@ import { createClientComponentClient } from '@supabase/auth-helpers-nextjs'
 
 export default function AuthForm() {
   const supabase = createClientComponentClient()
-
+  
   return (
     <Auth
       supabaseClient={supabase}
       view="magic_link"
-      appearance={{ theme: ThemeSupa }}
-      theme="dark"
       showLinks={false}
       providers={[]}
-      redirectTo="http://localhost:3000/auth/callback"
+      redirectTo={`${process.env.HOST}/auth/callback`}
     />
   )
 }
