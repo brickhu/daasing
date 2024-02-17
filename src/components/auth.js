14d0352c4671808b7af94ff971b2c29273927a07
Brickhu <brickhu@126.com>
2024-02-18 00:15:10 +0800

    服务端组件登录

--- src/components/auth.js
+++ src/components/auth.js
@@ -1,8 +1,9 @@
 'use client'
 
 import { Session, createClientComponentClient } from '@supabase/auth-helpers-nextjs';
-import { useEffect } from 'react';
-import { redirect } from 'next/navigation'
+import { useEffect,useState } from 'react';
+import { useRouter } from 'next/navigation'
+
 
 const userInfo = {
   email: 'brickhu@gmail.com',
@@ -12,11 +13,19 @@ const userInfo = {
 export default function Login({ session}){
   session = Session || null
   const supabase = createClientComponentClient()
+  const router = useRouter()
+
+  const test = async () => {
+    console.log(`${location.origin}/auth/callback`)
+  }
   
   const handleEmailSignup = async () => {
     let { data, error } = await supabase.auth.signUp({
       email: userInfo.email,
-      password: userInfo.password
+      password: userInfo.password,
+      options: {
+        emailRedirectTo: `${location.origin}/auth/callback`
+      }
     })
 
     if(error) {
@@ -25,6 +34,7 @@ export default function Login({ session}){
 
     if(data) {
       console.log(data)
+      router.refresh()
     }
   }
 
@@ -40,7 +50,7 @@ export default function Login({ session}){
 
     if(data) {
       console.log(data)
-      redirect(data.url)
+      session = data.session
     }
 	};
 
@@ -73,7 +83,8 @@ export default function Login({ session}){
 		<>
       <button onClick={handleEmailSignup}>注册</button><br/>
 			<button onClick={handleEmailLogin}>Email Login</button><br/>
-			<button onClick={handleGitHubLogin}>GitHub Login</button>
+			<button onClick={handleGitHubLogin}>GitHub Login</button><br/>
+      <button onClick={test}> test </button>
 		</>
 	);
 
--- src/middleware.js
+++ src/middleware.js
@@ -3,25 +3,25 @@ import { NextResponse } from 'next/server'
 
 export async function middleware(req) {
   const res = NextResponse.next()
+  // Create a Supabase client configured to use cookies
   const supabase = createMiddlewareClient({ req, res })
 
-  const {
-    data: { user },
-  } = await supabase.auth.getUser()
-
-  // if user is signed in and the current path is / redirect the user to /account
-  if (user && req.nextUrl.pathname === '/') {
-    return NextResponse.redirect(new URL('/account', req.url))
-  }
-
-  // if user is not signed in and the current path is not / redirect the user to /
-  if (!user && req.nextUrl.pathname !== '/') {
-    return NextResponse.redirect(new URL('/', req.url))
-  }
-
+  // Refresh session if expired - required for Server Components
+  const session = await supabase.auth.getSession()
+  console.log('执行中间件',session)
   return res
 }
 
+// Ensure the middleware is only called for relevant paths.
 export const config = {
-  matcher: ['/', '/account'],
+  matcher: [
+    /*
+     * Match all request paths except for the ones starting with:
+     * - _next/static (static files)
+     * - _next/image (image optimization files)
+     * - favicon.ico (favicon file)
+     * Feel free to modify this pattern to include more paths.
+     */
+    '/((?!_next/static|_next/image|favicon.ico).*)',
+  ],
 }
