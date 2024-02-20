cb1b2406b5665e9b69a4bbe9e428856b0e2484a2
Brickhu <brickhu@126.com>
2024-02-20 09:54:22 +0800

    Update auth.js

--- src/lib/auth.js
+++ src/lib/auth.js
@@ -1,14 +1,16 @@
 import { createServerComponentClient } from '@supabase/auth-helpers-nextjs'
 import { cookies } from 'next/headers'
-const cookieStore = cookies()
-const supabase = createServerComponentClient({ cookies: () => cookieStore })
 
 const getSession = async()=>{
+  const cookieStore = cookies()
+  const supabase = createServerComponentClient({ cookies: () => cookieStore })
   const {data:{session},error} = await supabase.auth.getSession()
   return ({session,error})
 }
 
 const getUser = async()=>{
+  const cookieStore = cookies()
+  const supabase = createServerComponentClient({ cookies: () => cookieStore })
   const {data:{user},error} = await supabase.auth.getUser()
   return ({user,error})
 }
