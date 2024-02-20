b7d0025d49fdd7ce5903a892982be1eb4240ddc0
Brickhu <brickhu@126.com>
2024-02-20 09:03:01 +0800

    修改首页page为servercomponent

--- src/app/page.js
+++ src/app/page.js
@@ -1,26 +1,14 @@
+import { createServerComponentClient } from '@supabase/auth-helpers-nextjs'
+import { cookies } from 'next/headers'
+export default async function Account() {
+  const cookieStore = cookies()
+  const supabase = createServerComponentClient({ cookies: () => cookieStore })
 
-"use client"
-// import { SessionProvider} from '@/components/session-context';
-// import dynamic from 'next/dynamic'
+  const {
+    data: { user },
+  } = await supabase.auth.getUser()
 
-// const Main = dynamic(()=> import('@/components/lazy-function'),{ssr:false,loading:()=><p>loading Main</p>})
-import {createClientComponentClient} from "@supabase/auth-helpers-nextjs"
-import { useEffect, useState } from "react"
-
-const supabase = createClientComponentClient()
-
-export default function Home() {
-  const [session,setSession] = useState()
-  useEffect(()=>async()=>{
-    const {data,error} = await supabase.auth.getUser()
-    console.log('data: ', data);
-    if(data){
-      setSession(data.user)
-    }
-  },[])
   return (
-    <div>
-      {session?<div>已登录</div>:<div>没有登录</div>}
-    </div>
+    <div>{user?user.email:"user unlogin"}</div>
   )
 }
