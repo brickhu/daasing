ac3915ed1f1b2fb9ffe2cc84bc0a2cd89d1f2f7b
Brickhu <brickhu@126.com>
2024-02-22 12:32:42 +0800

    Update route.js

--- src/app/api/note/get/route.js
+++ src/app/api/note/get/route.js
@@ -3,6 +3,7 @@ import { cookies } from 'next/headers'
 import { NextResponse } from 'next/server'
 
 export async function GET(request, context) {
+  'use server'
   try {
     cookies().getAll()
     const supabase = createRouteHandlerClient({cookies})
