cd8a1876c6e6ef457145f39dea1cd528ac9bf384
Brickhu <brickhu@126.com>
2024-02-22 09:17:00 +0800

    Update page.jsx

--- src/app/notes/page.jsx
+++ src/app/notes/page.jsx
@@ -12,7 +12,20 @@ export default function Notes(){
   const [notes,setNotes] = useState()
   const [loading,setLoading] = useState(true)
 
-  useEffect(()=>()=>fetchNotes({cache:"force-cache"}),[])
+  useEffect(() => async () => {
+    const response = await fetch('/api/note/get',{
+      method:"GET"
+    })
+    const {data,error} = await response.json()
+    if(error){
+      console.log(error)
+    }
+    if(data){
+      console.log('data: ', data);
+      setNotes(data)
+    }
+    setLoading(false)
+  },[])
 
   async function fetchNotes({cache}){
     const response = await fetch('/api/note/get',{
